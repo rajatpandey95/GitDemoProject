f0092a0cb9bc85fa731d3754699c5a695c9d2719
rajatpandey95 <rajat.pandey_cs19@gla.ac.in>
2023-06-13 23:49:14 +0530

    First commit...

---
+++ src/com/git/Test.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if(hungry.equals("yes")) {
+			System.out.println("Eat something...");
+		}
+		else {
+			System.out.println("Do work...");
+		}
+	}
+
+}
